9a549fd9c333ab28f315710f18e9912156b1efd9
way <1556080769@qq.com>
2025-09-25 17:11:46 +0800

    整合Animate.css做登录页动画

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from '@/App.vue'     // 引入 App.vue 组件
 import router from '@/router'  // 导入路由
 // 导入 Element Plus 图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'animate.css';
 // 创建应用，并将 App 根组件挂载到 <div id="#app"></div> 中‘
 const app = createApp(App)
 createApp(App).mount('#app')
--- src/pages/admin/login.vue
+++ src/pages/admin/login.vue
@@ -4,7 +4,8 @@
         <!-- 默认占两列，order 用于指定排列顺序，md 用于适配非移动端（PC 端） -->
         <div class="col-span-2 order-2 p-10 md:col-span-1 md:order-1 bg-slate-900">
             <!-- 指定为 flex 布局，并设置为屏幕垂直水平居中，高度为 100% -->
-            <div class="flex justify-center items-center h-full flex-col">
+            <div class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInLeft animate__fast
+">
                 <h2 class="font-bold text-4xl mb-7 text-white">Weblog 博客登录</h2>
                 <p class="text-white">一款由 Spring Boot + Mybaits Plus + Vue 3.2 + Vite 4 开发的前后端分离博客。</p>
                 <!-- 指定图片宽度为父级元素的 1/2 -->
@@ -13,7 +14,7 @@
         </div>
         <div class="col-span-2 order-1 md:col-span-1 md:order-2 bg-white">
             <!-- flex-col 用于指定子元素垂直排列 -->
-            <div class="flex justify-center items-center h-full flex-col">
+            <div class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInRight animate__fast">
                 <!-- 大标题，设置字体粗细、大小、下边距 -->
                 <h1 class="font-bold text-4xl mb-5">欢迎回来</h1>
                 <!-- 设置 flex 布局，内容垂直水平居中，文字颜色，以及子内容水平方向 x 轴间距 -->
