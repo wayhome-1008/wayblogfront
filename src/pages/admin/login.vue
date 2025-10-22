2ae645fcf9eb196468eb55e4c6324f7dc5fef317
way <1556080769@qq.com>
2025-10-22 17:32:32 +0800

    存储token到cookies

--- src/pages/admin/login.vue
+++ src/pages/admin/login.vue
@@ -4,7 +4,8 @@
         <!-- 默认占两列，order 用于指定排列顺序，md 用于适配非移动端（PC 端） -->
         <div class="col-span-2 order-2 p-10 md:col-span-1 md:order-1 bg-slate-900">
             <!-- 指定为 flex 布局，并设置为屏幕垂直水平居中，高度为 100% -->
-            <div class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInLeft animate__fast">
+            <div
+                class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInLeft animate__fast">
                 <h2 class="font-bold text-4xl mb-7 text-white">Weblog 博客登录</h2>
                 <p class="text-white">一款由 Spring Boot + Mybaits Plus + Vue 3.2 + Vite 4 开发的前后端分离博客。</p>
                 <!-- 指定图片宽度为父级元素的 1/2 -->
@@ -13,7 +14,8 @@
         </div>
         <div class="col-span-2 order-1 md:col-span-1 md:order-2 bg-white">
             <!-- flex-col 用于指定子元素垂直排列 -->
-            <div class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInRight animate__fast">
+            <div
+                class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInRight animate__fast">
                 <!-- 大标题，设置字体粗细、大小、下边距 -->
                 <h1 class="font-bold text-4xl mb-5">欢迎回来</h1>
                 <!-- 设置 flex 布局，内容垂直水平居中，文字颜色，以及子内容水平方向 x 轴间距 -->
@@ -28,7 +30,8 @@
                 <el-form class="w-5/6 md:w-2/5" ref="formRef" :rules="rules" :model="form">
                     <el-form-item prop="username">
                         <!-- 输入框组件 -->
-                        <el-input size="large" v-model="form.username" placeholder="请输入用户名" :prefix-icon="User" clearable />
+                        <el-input size="large" v-model="form.username" placeholder="请输入用户名" :prefix-icon="User"
+                            clearable />
                     </el-form-item>
                     <el-form-item prop="password">
                         <!-- 密码框组件 -->
@@ -37,7 +40,8 @@
                     </el-form-item>
                     <el-form-item>
                         <!-- 登录按钮，宽度设置为 100% -->
-                        <el-button class="w-full mt-2" size="large" :loading="loading" type="primary" @click="onSubmit">登录</el-button>
+                        <el-button class="w-full mt-2" size="large" :loading="loading" type="primary"
+                            @click="onSubmit">登录</el-button>
                     </el-form-item>
                 </el-form>
 
@@ -52,7 +56,8 @@ import { User, Lock } from '@element-plus/icons-vue'
 import { login } from '@/api/admin/user'
 import { ref, reactive, onMounted, onBeforeUnmount } from 'vue'
 import { useRouter } from 'vue-router';
-import { showMessage} from '@/composables/util'
+import { showMessage } from '@/composables/util'
+import { setToken } from '@/composables/auth'
 // 按回车键后，执行登录事件
 function onKeyUp(e) {
     console.log(e)
@@ -115,21 +120,23 @@ const onSubmit = () => {
             // 判断是否成功
             // 判断是否成功
             if (res.data.success == true) {
-            	 // 提示登录成功
+                // 提示登录成功
                 showMessage('登录成功')
-
+                // 存储 Token 到 Cookie 中
+                let token = res.data.data.token
+                setToken(token)
                 // 跳转到后台首页
                 router.push('/admin/index')
             } else {
-               let message = res.data.message
+                let message = res.data.message
                 // 提示消息
                 showMessage(message, 'error')
             }
         })
-        .finally(() => {
-            // 结束加载
-            loading.value = false
-        })
+            .finally(() => {
+                // 结束加载
+                loading.value = false
+            })
     })
 }
 
