352b1c339ee50499df5487c4823f58c2845680a4
way <1556080769@qq.com>
2025-10-23 17:02:12 +0800

    页面标题设置、后台是否登录跳转

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 // 导入路由
 import router from '@/router'
+// 导入全局路由守卫
+import '@/permission'
 // 导入 Element Plus 图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
