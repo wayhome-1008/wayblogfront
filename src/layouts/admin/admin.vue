79d2bcca13650f33641b480770f08451eba71107
way <1556080769@qq.com>
2025-10-28 17:34:30 +0800

    全局过渡动画

--- src/layouts/admin/admin.vue
+++ src/layouts/admin/admin.vue
@@ -17,9 +17,18 @@
             <el-main>
                 <!-- 标签导航栏 -->
                 <AdminTagList></AdminTagList>
+				<!-- 主内容（根据路由动态展示不同页面） -->
+                <router-view v-slot="{ Component }">
+                    <Transition name="fade">
+                        <!-- max 指定最多缓存 10 个组件 -->
+                        <KeepAlive :max="10">
+                            <component :is="Component"></component>
+                        </KeepAlive>
+                    </Transition>
+                    
+                </router-view>
 
-                <!-- 主内容（根据路由动态展示不同页面） -->
-                <router-view></router-view>
+
             </el-main>
 
             <!-- 底栏容器 -->
@@ -43,7 +52,41 @@ const menuStore = useMenuStore()
 .el-header {
     padding: 0 !important;
 }
+
 .el-footer {
-    padding: 0!important;
+    padding: 0 !important;
+}
+
+/* 内容区域过渡动画：淡入淡出效果 */
+/* 刚开始进入时 */
+.fade-enter-from {
+    /* 透明度 */
+    opacity: 0;
+}
+
+/* 刚开始结束 */
+.fade-enter-to {
+    opacity: 1;
+}
+
+/* 刚开始离开 */
+.fade-leave-from {
+  opacity: 1;
+}
+
+/* 离开已结束 */
+.fade-leave-to {
+  opacity: 0;
+}
+
+/* 离开进行中 */
+.fade-leave-active {
+    transition: all 0.3s;
+}
+
+/* 进入进行中 */
+.fade-enter-active {
+    transition: all 0.3s;
+    transition-delay: 0.3s;
 }
 </style>
