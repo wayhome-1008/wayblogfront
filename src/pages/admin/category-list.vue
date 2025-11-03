b52a6f0de3a83b3562e5ad520a381ff882f6028f
way <1556080769@qq.com>
2025-11-03 17:05:48 +0800

    文章分类删除

--- src/pages/admin/category-list.vue
+++ src/pages/admin/category-list.vue
@@ -35,12 +35,12 @@
                 <el-table-column prop="createTime" label="创建时间" width="180" />
                 <el-table-column label="操作">
                     <template #default="scope">
-                        <el-button type="danger" size="small">
-                            <el-icon class="mr-1">
-                                <Delete />
-                            </el-icon>
-                            删除
-                        </el-button>
+                        <el-button type="danger" size="small" @click="deleteCategorySubmit(scope.row)">
+                        <el-icon class="mr-1">
+                            <Delete />
+                        </el-icon>
+                        删除
+                    </el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -78,9 +78,9 @@
 // 引入所需图标
 import { Search, RefreshRight } from '@element-plus/icons-vue'
 import { ref, reactive } from 'vue'
-import { getCategoryPageList, addCategory } from '@/api/admin/category'
+import { getCategoryPageList, addCategory, deleteCategory } from '@/api/admin/category'
 import moment from 'moment'
-import { showMessage } from '@/composables/util'
+import { showMessage, showModel } from '@/composables/util'
 // 对话框是否显示
 const dialogVisible = ref(false)
 // 表单引用
@@ -211,5 +211,24 @@ const reset = () => {
     pickDate.value = ''
     startDate.value = null
     endDate.value = null
+}
+const deleteCategorySubmit = (row) => {
+    console.log(row.id)
+    showModel('是否确定要删除该分类？').then(() => {
+        deleteCategory(row.id).then((res) => {
+            if (res.success == true) {
+                showMessage('删除成功')
+                // 重新请求分页接口，渲染数据
+                getTableData()
+            } else {
+                // 获取服务端返回的错误消息
+                let message = res.message
+                // 提示错误消息
+                showMessage(message, 'error')
+            }
+        })
+    }).catch(() => {
+        console.log('取消了')
+    })
 }
 </script>
