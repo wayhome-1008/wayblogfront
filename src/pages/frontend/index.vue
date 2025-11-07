2cc590410f6c7c0a4d2f0b365d3a5522d1843aef
way <1556080769@qq.com>
2025-11-07 15:51:59 +0800

    首页样式设计

--- src/pages/frontend/index.vue
+++ src/pages/frontend/index.vue
@@ -1,6 +1,146 @@
 <template>
     <Header></Header>
+
+    <!-- 主内容区域 -->
+    <main class="container max-w-screen-xl mx-auto p-4">
+        <!-- grid 表格布局，分为 4 列 -->
+        <div class="grid grid-cols-4 gap-4">
+            <!-- 左边栏，占用 3 列 -->
+            <div class="col-span-4 md:col-span-3">
+                <!-- 文章列表，grid 表格布局，分为 2 列 -->
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="col-span-2 md:col-span-1">
+                        <div class="bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <!-- 文章封面 -->
+                            <a href="#">
+                                <img class="rounded-t-lg h-48 w-full"
+                                    src="https://img.quanxiaoha.com/quanxiaoha/193dd1504ebb4f138085acb23619e0dd.jpg"
+                                    alt="" />
+                            </a>
+                            <div class="p-5">
+                                <!-- 标签 -->
+                                <div class="mb-3">
+                                    <span
+                                        class="cursor-pointer bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded hover:bg-green-200 hover:text-green-900 dark:bg-green-900 dark:text-green-300">从0到1</span>
+                                    <span
+                                        class="cursor-pointer bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded hover:bg-green-200 hover:text-green-900 dark:bg-green-900 dark:text-green-300">知识星球</span>
+                                    <span
+                                        class="cursor-pointer bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded hover:bg-green-200 hover:text-green-900 dark:bg-green-900 dark:text-green-300">项目实战</span>
+                                </div>
+
+                                <!-- 文章标题 -->
+                                <a href="#">
+                                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        从零手撸前后端分离博客（已更新11w+字）</h2>
+                                </a>
+                                <!-- 文章摘要 -->
+                                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">已更新 69 节内容，共计 105150
+                                    字，演示截图：521
+                                    张，持续爆肝中...</p>
+                                <!-- 文章发布时间、所属分类 -->
+                                <p class="flex items-center font-normal text-gray-400 text-sm dark:text-gray-400">
+                                    <!-- 发布时间 -->
+                                    <svg class="inline w-3 h-3 mr-2 text-gray-400 dark:text-white" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M5 1v3m5-3v3m5-3v3M1 7h18M5 11h10M2 3h16a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Z" />
+                                    </svg>
+                                    2023-10-22
+
+                                    <!-- 所属分类 -->
+                                    <svg class="inline w-3 h-3 ml-5 mr-2 text-gray-400 dark:text-white"
+                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 18 18">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M1 5v11a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H1Zm0 0V2a1 1 0 0 1 1-1h5.443a1 1 0 0 1 .8.4l2.7 3.6H1Z" />
+                                    </svg>
+                                    <a href="#" class="text-gray-400 hover:underline">Java</a>
+                                </p>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-span-2 md:col-span-1">
+                        <div class="bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img class="rounded-t-lg h-48 w-full"
+                                    src="https://img.quanxiaoha.com/quanxiaoha/193dd1504ebb4f138085acb23619e0dd.jpg"
+                                    alt="" />
+                            </a>
+                            <div class="p-5">
+                                <a href="#">
+                                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        从零手撸前后端分离博客（已更新11w+字）</h2>
+                                </a>
+                                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">已更新 69 节内容，共计 105150
+                                    字，演示截图：521
+                                    张，持续爆肝中...</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- 分页 -->
+                <nav aria-label="Page navigation example" class="mt-10 flex justify-center">
+                    <ul class="flex items-center -space-x-px h-10 text-base">
+                        <li>
+                            <a href="#"
+                                class="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                                <span class="sr-only">Previous</span>
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M5 1 1 5l4 4" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                        </li>
+                        <li>
+                            <a href="#" aria-current="page"
+                                class="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                                <span class="sr-only">Next</span>
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 9 4-4-4-4" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+
+            <!-- 右边侧边栏，占用一列 -->
+            <aside class="col-span-4 bg-green-500 md:col-span-1">右边栏</aside>
+        </div>
+
+    </main>
 </template>
+
 
 <script setup>
 import Header from '@/layouts/frontend/components/Header.vue'
